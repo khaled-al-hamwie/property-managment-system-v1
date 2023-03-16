bd3ba5fc6d83527a783ee25e1405906c12268c08
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-16 22:07:20 +0300

    adding database core utilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from "./core/database/database.module";
+import { PropertyModule } from "./property/property.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		PropertyModule,
+		DatabaseModule,
+	],
 })
 export class AppModule {}
