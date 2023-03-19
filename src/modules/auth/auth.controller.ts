609e6f3fe21b92b99e6f72d28222f5b0c01d0b8f
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-19 23:55:33 +0300

    adding the return type of the controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,18 +3,19 @@ import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { registerDto } from "./dto/register.dto";
 import { LocalAuthGuard } from "./guard/local-auth.guard";
+import { JwtAttributes } from "./interfaces/jwt.interface";
 
 @Controller("")
 export class AuthController {
 	constructor(private authService: AuthService) {}
 	@Post("/register")
-	async register(@Body() body: registerDto) {
+	async register(@Body() body: registerDto): Promise<JwtAttributes> {
 		return await this.authService.register(body);
 	}
 
 	@UseGuards(LocalAuthGuard)
 	@Post("/login")
-	async logIn(@Request() req) {
+	async logIn(@Request() req): Promise<JwtAttributes> {
 		return await this.authService.login(req.user);
 	}
 }
