a2696d90b4148905a3085fcc5ac99f3145536677
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-21 21:47:06 +0300

    moving the country and city module to location

--- src/modules/locations/locations.module.ts
+++ src/modules/locations/locations.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { CitiesModule } from "../cities/cities.module";
+import { CountriesModule } from "../countries/countries.module";
 import { Location } from "./location.entity";
 import { LocationsService } from "./locations.service";
 @Module({
-	imports: [SequelizeModule.forFeature([Location])],
+	imports: [
+		SequelizeModule.forFeature([Location]),
+		CitiesModule,
+		CountriesModule,
+	],
 	providers: [LocationsService],
 	exports: [LocationsService],
 })
--- src/modules/locations/locations.service.ts
+++ src/modules/locations/locations.service.ts
@@ -1,14 +1,30 @@
-import { Injectable, Module } from "@nestjs/common";
+import { ForbiddenException, Injectable, Module } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
+import { CitiesService } from "../cities/cities.service";
+import { CountriesService } from "../countries/countries.service";
 import { LocationCreation } from "./interfaces/location.entity.interface";
 import { Location } from "./location.entity";
 
 @Injectable()
 export class LocationsService {
 	constructor(
-		@InjectModel(Location) private LocationModel: typeof Location
+		@InjectModel(Location) private LocationModel: typeof Location,
+		private City: CitiesService,
+		private Country: CountriesService
 	) {}
-	async add(location: LocationCreation): Promise<Location> {
+	async create(location: LocationCreation): Promise<Location> {
+		const city = await this.City.find(location.city_id);
+		const country = await this.Country.find(location.country_id);
+		if (!city) {
+			throw new ForbiddenException("city_id unvalid city", {
+				description: "Forbidden",
+			});
+		}
+		if (!country) {
+			throw new ForbiddenException("country_id unvalid country", {
+				description: "Forbidden",
+			});
+		}
 		return await this.LocationModel.create(location);
 	}
 }
