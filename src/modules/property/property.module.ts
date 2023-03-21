e0c7b7f753521af2004eaee91d2badbcd9ba326e
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-21 15:43:00 +0300

    importing the city and country module

--- src/modules/property/property.module.ts
+++ src/modules/property/property.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { CitiesModule } from "../cities/cities.module";
+import { CountriesModule } from "../countries/countries.module";
 import { LocationsModule } from "../locations/locations.module";
 import { PropertyController } from "./property.controller";
 import { PropertyService } from "./property.service";
 
 @Module({
-	imports: [LocationsModule],
+	imports: [LocationsModule, CitiesModule, CountriesModule],
 	controllers: [PropertyController],
 	providers: [PropertyService],
 })
