e4d82ec089cecc018ff1eb54b6dbf011b55f01cb
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-21 21:43:55 +0300

    removing un used import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,14 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { databaseProvider } from "./core/database/database.providers";
 import { AuthModule } from "./modules/auth/auth.module";
-import { CitiesModule } from "./modules/cities/cities.module";
-import { CountriesModule } from "./modules/countries/countries.module";
-import { CredentialsModule } from "./modules/credentials/credentials.module";
 import { PropertyModule } from "./modules/property/property.module";
-import { UsersModule } from "./modules/users/users.module";
-import { PropertyTypesModule } from './modules/property-types/property-types.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		databaseProvider,
-		UsersModule,
 		PropertyModule,
-		CredentialsModule,
 		AuthModule,
-		CitiesModule,
-		CountriesModule,
-		PropertyTypesModule,
 	],
 })
 export class AppModule {}
