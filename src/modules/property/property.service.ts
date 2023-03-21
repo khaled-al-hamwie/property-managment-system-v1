0450215df096ae459b2159cb42fcffd329081f0c
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-03-21 15:43:31 +0300

    adding the add location functionality

--- src/modules/property/property.service.ts
+++ src/modules/property/property.service.ts
@@ -1,10 +1,16 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { CitiesService } from "../cities/cities.service";
+import { CountriesService } from "../countries/countries.service";
 import { LocationCreation } from "../locations/interfaces/location.entity.interface";
 import { LocationsService } from "../locations/locations.service";
 
 @Injectable()
 export class PropertyService {
-	constructor(private Location: LocationsService) {}
+	constructor(
+		private Location: LocationsService,
+		private City: CitiesService,
+		private Country: CountriesService
+	) {}
 	getProperties() {
 		return "fjds;";
 	}
@@ -12,11 +18,16 @@ export class PropertyService {
 	async createProperty() {
 		// add location
 		let location: LocationCreation = {
-			country_id: 0,
-			city_id: 0,
-			place: "",
+			country_id: 1,
+			city_id: 1,
+			place: "in the La Rambla next to the pitza hat",
 		};
-		// let country=
+		// check if country and city exist
+		const city = await this.City.find(location.city_id);
+		const country = await this.Country.find(location.country_id);
+		if (!city || !country) {
+			return "invalid city or country";
+		}
 		await this.Location.add(location);
 		return "hi";
 	}
