5dc2660ab45ae199efef5104f9f8f3e8ee0d5b8a
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-01 01:35:03 +0300

    adding the constants folder

---
+++ test/city/constants/index.ts
@@ -0,0 +1,32 @@
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "src/app.module";
+import { AuthService } from "src/modules/auth/auth.service";
+import { AdminPayload } from "src/modules/auth/interfaces/payload.interface";
+
+export const route = "/admin/city";
+export const loginBody: AdminPayload = {
+	admin_id: 1,
+	user_name: "testseeder1",
+};
+
+export async function setUp() {
+	let app: INestApplication;
+	let authService: AuthService;
+	let token: string;
+	const moduleFixture: TestingModule = await Test.createTestingModule({
+		imports: [AppModule],
+	}).compile();
+	app = moduleFixture.createNestApplication();
+	authService = moduleFixture.get<AuthService>(AuthService);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			stopAtFirstError: true,
+			errorHttpStatusCode: HttpStatus.FORBIDDEN,
+		})
+	);
+	await app.init();
+	token = (await authService.login(loginBody)).access_token;
+	return { app, authService, token };
+}
