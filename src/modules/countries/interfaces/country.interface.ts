a4131d3224162a8566c25ce0e54c16b990f3eb1d
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-01 01:03:19 +0300

    adding the currency and iso to the country interface

--- src/modules/countries/interfaces/country.interface.ts
+++ src/modules/countries/interfaces/country.interface.ts
@@ -3,7 +3,8 @@ import { Optional } from "sequelize";
 export interface CountryAttributes {
 	country_id: number;
 	name: string;
-	state?: string;
+	iso: string;
+	currency: string;
 }
 
 export interface CountryCreationAttributes
